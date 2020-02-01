aa537f4bf8d7d3c63aa24e3e9eab678a769b6083
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-01 02:57:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort do |left, right|
+    right <=> left
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end 
+
+def swap_elements(array)
+  array [1], array [2] = array [2], array [1]
+  array
+end 
+#array swap by index
+
+def reverse_array(array)
+    array.reverse 
+end 
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$" #replace third letter with dollar sign
+  end
+end 
+
+def find_a(array)
+array.find_all do |word|
+    word[0] == "a" #find letter starting with a. first letter in index is 0.
+  end
+end 
+
+sum_array(array)
